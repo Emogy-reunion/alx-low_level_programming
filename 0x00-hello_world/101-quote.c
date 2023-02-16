a5575f233fffae1545b8d528871853b79c2c3a1d
Emogy_reunion <mv7786986@gmail.com>
2023-02-16 07:50:39 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - Program that prints a function followed by a new line to the standard header
+ *Return: 1 (Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
