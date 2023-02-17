cd37dbae9ea08777df2e0da85fbf7e9ac35e03ce
Emogy_reunion <mv7786986@gmail.com>
2023-02-17 08:15:44 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Program to print alphabet in reverse
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
