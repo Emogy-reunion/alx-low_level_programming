97636219a48ac8370b91a4af0116b43a1b97d02f
Emogy_reunion <mv7786986@gmail.com>
2023-02-17 07:53:43 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - P rogram that prints 0 to 9
+ * Return: 0 (SUCCESS)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
