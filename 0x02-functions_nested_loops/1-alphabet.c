81021d45fed53f2e3b689fd426890fd5f552cfde
Emogy_reunion <mv7786986@gmail.com>
2023-02-22 00:48:19 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - program that prints alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
