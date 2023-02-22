0329954b9bf4dc6eec66db5c35e47109ea587af8
Emogy_reunion <mv7786986@gmail.com>
2023-02-22 07:20:20 -0800

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints even terms and a new line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	unsigned long int m, n, next, sum;
+
+	m = 1;
+	n = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
+	{
+		if(m < 4000000 && (m % 2) == 0)
+		{
+			sum += m;
+		}
+		next = m + n;
+		m = n;
+		n = next;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
