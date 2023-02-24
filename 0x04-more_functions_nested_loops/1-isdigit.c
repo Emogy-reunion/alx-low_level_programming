4b1d2abc3f9a71b902a954dfd90f0461636a1009
Emogy_reunion <mv7786986@gmail.com>
2023-02-24 06:14:16 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit -  checks for checks for a digit (0 through 9).
+ * @c: c - int to be checked
+ * Return:  0 (success)
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
