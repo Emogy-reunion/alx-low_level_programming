141d4c27a8cc1d2f677f818f5657d608af1d245d
Emogy_reunion <mv7786986@gmail.com>
2023-02-27 12:22:00 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer and updates the value it points to.
+ * @n: declaration of n and parameter for the n function.
+ *
+ * Return: Nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
