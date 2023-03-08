64e8dd5b98dca43e0eed4ec33b45321ca06d9b60
Emogy_reunion <mv7786986@gmail.com>
2023-03-08 14:23:21 -0800

    first commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion -  prints a reverse string
+ * @s: string to be reversed
+ *
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
