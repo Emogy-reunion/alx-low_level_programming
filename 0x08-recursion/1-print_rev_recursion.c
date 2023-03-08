b3ec897e3dfc62f8c2ed6ff0909a8a251b887b5a
Emogy_reunion <mv7786986@gmail.com>
2023-03-08 14:28:51 -0800

    first commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,10 +11,10 @@ void _print_rev_recursion(char *s)
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
-		_putchar(*s);
 	}
 	else
 	{
-	_putchar('\n');
+		_putchar(*s);
 	}
+	_putchar('\n');
 }
