40394ef393c0328994d40c07efa2a6614bc7ce03
Emogy_reunion <mv7786986@gmail.com>
2023-03-08 11:00:48 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,10 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		return;
-	}
-	else
-	{
-		_putchar(s);
+	if (*s != '\0')
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	else
 	_putchar('\n');
 }
