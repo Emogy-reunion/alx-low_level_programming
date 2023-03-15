261b7447d6a8f2c2c955dbf199b653528b87eef1
Emogy_reunion <mv7786986@gmail.com>
2023-03-15 03:24:41 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,16 +12,16 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
-	int i;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 	arr = (char *)malloc(sizeof(char) * size);
 	if (arr == NULL)
 		return (0);
-	for (i = 0, i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		arr[i] = 'c';
+		arr[i] = c;
 	}
 	return (arr);
 }
