6c99ea675b041c4128f3d575beba195b30ee64b1
Emogy_reunion <mv7786986@gmail.com>
2023-03-21 01:30:35 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,12 @@
 #define DOG_H
 
 /**
- * struct dog - a dog'd basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * struct dog - struct dog
+ * @name: the first member
+ * @age: the econd member
+ * @owner: the third member
  *
- * Description: Longer description
+ * Description: this struct is for dog
  */
 struct dog
 {
@@ -25,7 +25,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif /*DOG_H*/
