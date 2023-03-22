bf5274433a8ede96373f8da1367f31e7e90ba431
Emogy_reunion <mv7786986@gmail.com>
2023-03-22 16:39:50 -0700

    commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,14 +10,14 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int m;
 
-	if (arry == NULL || size <= 0 || cmp == NULL)
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
-	for (i = 0; i < size; i++)
+	for (m = 0; m < size; m++)
 	{
-		if (cmp(array[i]))
-			return (i);
+		if (cmp(array[m]))
+			return (m);
 	}
 	return (-1);
 }
