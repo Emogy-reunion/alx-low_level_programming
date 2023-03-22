d2d06828c31fe4d2d3bf62750de820bd9ba3a659
Emogy_reunion <mv7786986@gmail.com>
2023-03-22 16:20:13 -0700

    commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdio.h>
+#include <stddef.h>
 
 /**
  * int_index - returns the index of the first element
@@ -13,14 +13,19 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int index;
+	bool y;
 
-	if (array == NULL || size <= 0 || cmp == NULL)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (array != NULL && size > 0 && cmp != NULL)
 	{
-		if (cmp(array[i]));
-		return (i);
+		if (size <= 0)
+			return (-1);
+		for (index = 0; index < size; index++)
+		{
+			y = cmp(array[index]);
+			if (y == TRUE)
+				return (index);
+		}
 	}
 	return (-1);
 }
