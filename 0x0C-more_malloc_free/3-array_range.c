5d62e47c79318e03d79babcc23bc4d6f76295033
Emogy-reunion <mv7786986@gmail.com>
2023-03-29 02:08:41 -0700

    commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -9,22 +10,19 @@
  */
 int *array_range(int min, int max)
 {
-	int *block;
-	int i, j = 0;
+	int *ptr;
+	int i, size;
 
 	if (min > max)
 		return (NULL);
 
-	block = malloc(sizeof(*block) * ((max - min + 1));
-	if (block != NULL)
-	{
-		for (i = min; i <= max; i++)
-		{
-			block[j] = i;
-			j++;
-		}
-		return (block);
-	}
-	else
+	size = max - min + 1;
+
+	ptr = malloc(sizeof(int) * size);
+
+	if (ptr == NULL)
 		return (NULL);
+	for (i = 0; min <= max; i++)
+		ptr[i] = min++;
+	return (ptr);
 }
