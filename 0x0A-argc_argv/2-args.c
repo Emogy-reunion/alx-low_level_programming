a3c4088359f74da8b7ebdd495367eff743963f1d
Emogy-reunion <mv7786986@gmail.com>
2023-03-29 00:57:09 -0700

    commit

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - a program that prints all arguments it receives
@@ -14,6 +13,8 @@ int main(int argc, char *argv[])
 	int i;
 
 	for (i = 0; i < argc; i++)
+	{
 		printf("%s\n", argv[i]);
-	printf("%s\n", argv[i]);
+	}
+	return (0);
 }
