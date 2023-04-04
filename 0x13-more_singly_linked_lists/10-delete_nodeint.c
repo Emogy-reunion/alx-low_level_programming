c4aa6cbe62260ad4c0a100671414db3cc4b27ab8
Emogy-reunion <mv7786986@gmail.com>
2023-04-04 10:28:36 -0700

    commit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,4 +22,16 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
+		while (i < index - 1)
+	{
+		if (!temp || !(temp->next))
+		return (-1);
+		temp = temp->next;
+		i++;
+	}
+
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
+	return (1);
 }
