985ada21f142ed7b50eb766e3e2e126fe70522ab
Emogy-reunion <mv7786986@gmail.com>
2023-04-11 15:00:26 -0700

    commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * binary_to_uint - function that Converts binary number to an unsigned int
  * @b: string to be converted
@@ -16,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-			result = 2 * result + (b[i] - '0');
+	result = 2 * result + (b[i] - '0');
 	}
 	return (result);
 }
