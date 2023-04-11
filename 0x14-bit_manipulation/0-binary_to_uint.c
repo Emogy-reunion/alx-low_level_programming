24fd547d84a92fd81ceacb087375b191b0e8a708
Emogy-reunion <mv7786986@gmail.com>
2023-04-11 14:46:04 -0700

    first commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,22 +12,15 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	unsigned int base = 1;
 	int m;
 
 	if (b == NULL)
 		return (0);
-	for (m = 0; b[m]  != '\0'; m++)
+	for (m = 0; b[m]; m++)
 	{
-		if (b[m] != '0' && b[m] != '1')
+		if (b[m] != '0' || b[m] != '1')
 			return (0);
-		base <<= 1;
-	}
-	for (--m; m >= 0; m--)
-	{
-		if (b[m] == '1')
-			num += base;
-		base <<= 1;
+		num = 2 * num + (b[m] - '0');
 	}
 	return (num);
 }
