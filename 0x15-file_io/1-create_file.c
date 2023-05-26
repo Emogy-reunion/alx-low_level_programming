f65d39a89199ae33fb5a8948eda984ab2179cf93
Emogy-reunion <mv7786986@gmail.com>
2023-05-26 05:27:46 -0700

    commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,13 @@
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
