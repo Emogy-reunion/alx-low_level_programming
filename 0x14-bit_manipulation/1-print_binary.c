cf9ef42ef80211308ace87c3ca6823bda1167d2b
Emogy-reunion <mv7786986@gmail.com>
2023-08-31 15:08:49 -0700

    modified code not to use array

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_binary - prints binary represantion of a number
+ * print_binary - prints binary representation of a number
  * @n: number to print in binary
  *
  * Return: nothing
@@ -9,28 +9,27 @@
 
 void print_binary(unsigned long int n)
 {
-	static char binary[33];
-	int m;
-	int x;
-	int located_first_one;
+        int shift = sizeof(n) * 8 - 1;
+        int leading_zeros = 1;
 
-	if (n == 0)
-		_putchar('0');
+        while (shift >= 0)
+        {
+                if ((n >> shift) & 1)
+                {
+                        leading_zeros = 0;
+                        _putchar('1');
+                }
+                else if (!leading_zeros)
+                {
+                        _putchar('0');
+                }
 
-	for (m = 0; m < 32; m++)
-	{
-		binary[m] = (n & 0x80000000) ? '1' : '0';
-		n <<= 1;
-	}
-	binary[m] = '\0';
+                shift--;
+        }
 
-	located_first_one = 0;
-	for (x = 0; x < 33; x++)
-	{
-		if (binary[x] == '1')
-			located_first_one = 1;
-		if (located_first_one)
-			_putchar(binary[x]);
-	}
-	_putchar('\n');
+        if (leading_zeros)
+                _putchar('0');
+
+        _putchar('\n');
 }
+
