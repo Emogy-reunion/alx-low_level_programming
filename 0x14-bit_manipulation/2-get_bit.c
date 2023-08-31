11455961c9f436b6d6c03ca7db8e18c5a28703aa
Emogy-reunion <mv7786986@gmail.com>
2023-08-31 16:32:06 -0700

    first commit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - returns value of a bit at a given index
+ * @n: no to extract bit
+ * @index: index of the bit you want to get
+ *
+ * Return: bit at the value or -1 if error occurredd
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int value;
+
+	value = ( n >> index) & 1;
+	
+	if (index > 63)
+		return (-1);
+	return (value);
+}
