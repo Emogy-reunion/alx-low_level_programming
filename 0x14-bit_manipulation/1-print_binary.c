4cda6f19213e0ddf4d615f57782add956d7f1ec8
Emogy-reunion <mv7786986@gmail.com>
2023-08-31 15:18:00 -0700

    first commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,27 +9,23 @@
 
 void print_binary(unsigned long int n)
 {
-        int shift = sizeof(n) * 8 - 1;
-        int leading_zeros = 1;
+	int shift = sizeof(n) * 8 - 1;
+	int leading_zeros = 1;
 
-        while (shift >= 0)
-        {
-                if ((n >> shift) & 1)
-                {
-                        leading_zeros = 0;
-                        _putchar('1');
-                }
-                else if (!leading_zeros)
-                {
-                        _putchar('0');
-                }
-
-                shift--;
-        }
-
-        if (leading_zeros)
-                _putchar('0');
-
-        _putchar('\n');
+	while (shift >= 0)
+	{
+		if ((n >> shift) & 1)
+		{
+			leading_zeros = 0;
+			_putchar('1');
+		}
+		else if (!leading_zeros)
+		{
+			_putchar('0');
+		}
+		shift--;
+	}
+	if (leading_zeros)
+		_putchar('0');
+	_putchar('\n');
 }
-
