5f208544487a56cd5efd587db3b1ddf5323de1d5
Emogy-reunion <mv7786986@gmail.com>
2023-08-31 17:05:59 -0700

    first commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,8 +12,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int value;
 
-	value = (n >> index) & 1;
 	if (index > 63)
 		return (-1);
+	value = (n >> index) & 1;
 	return (value);
 }
