2ddbe8222a85a7ea0da96a9bc73f720bccb3da33
Emogy-reunion <mv7786986@gmail.com>
2023-08-31 17:40:50 -0700

    first commit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,7 +4,7 @@
  * set_bit - set the value of a bit to 1 at a given index
  * @n: pointer to value
  * @index: bit to set
- * Return: 1 iff it woorked or -1 iff it didn't
+ * Return: 1 if it woorked or -1 iff it didn't
  */
 int set_bit(unsigned long int *n, unsigned int index);
 {
