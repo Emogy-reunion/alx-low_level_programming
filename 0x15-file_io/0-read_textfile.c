d47ca5d05c0fa340b15b9d218319a04b3b47249b
Emogy-reunion <mv7786986@gmail.com>
2023-09-04 16:26:37 -0700

    first commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	filede = open(filename, O_RDONLY);
 	if (filede == -1)
 		return (0);
-	
+
 	stor = malloc(sizeof(char) * letters);
 	if (stor == NULL)
 		return (0);
