63a276f33daf5a9f3db0aae7e03c6a151a422d1f
Emogy-reunion <mv7786986@gmail.com>
2023-09-28 03:06:50 -0700

    first commit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_bit - set the value of a bit to 1 at a given index
+ * @n: pointer to value
+ * @index: bit to set
+ * Return: 1 if it woorked or -1 iff it didn't
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n = ((1UL << index) | *n);
+	return (1);
+}
